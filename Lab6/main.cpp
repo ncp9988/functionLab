7ba39233c87554531e58a250016c4232f57be218
ncp9988 <ncp9988@gmail.com>
2022-10-20 13:55:41 -0500

    zeroSmall function

--- Lab6/main.cpp
+++ Lab6/main.cpp
@@ -15,7 +15,8 @@ using namespace std;
 
 int Summation ( int num1, int num2);
 double raisePower (double n, int p);
-bool isEven ( int a, int b);
+bool isEven ( int a);
+void ZeroSmall (int& a, int& b, int& c);
 
 
 
@@ -34,10 +35,17 @@ int main() {
     cout <<"The square of "<< n << " is "<< raisePower(n , p)<< endl;
     
     
-    int a, b;
-    cout << "Enter 2 number to find if they are even:"<< endl;
-    cin >> a >> b;
-    cout << a << " and "<< b << " are even number: "<< isEven(a,b)<< endl;
+    int a;
+    cout << "Enter a number to find if it is an even number"<< endl;
+    cin >> a ;
+    cout << a << " is an even number: "<< isEven(a)<< endl;
+    
+    
+    
+    
+    int number1 = 9, number2= 10, number3= 2;
+    ZeroSmall ( number1, number2, number3);
+    cout << number1 << " "<< number2 << " " << number3<< endl;
 
     
     
@@ -73,9 +81,21 @@ double raisePower( double n, int p)
 
 
 //isEven
-bool isEven ( int a, int b)
+bool isEven ( int a)
 {
-    if (a==b)
+    if (a % 2 == 0)
         return true;
     return false;
 }
+
+void ZeroSmall (int& a, int& b, int& c)
+{
+    if (a<b && a<c)
+        a=0;
+    else if (b<c)
+        b=0;
+    else
+        c=0;
+}
+
+
