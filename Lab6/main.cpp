752fe870d5a07cf64874f6b608da00a78880163b
ncp9988 <ncp9988@gmail.com>
2022-10-20 11:17:15 -0500

    Initial Commit

---
+++ Lab6/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab6
+//
+//  Created by Chau Pham on 10/20/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
