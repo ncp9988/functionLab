30c7e19dcf8eab4bad25c80f6102dcfd8084c7b1
ncp9988 <ncp9988@gmail.com>
2022-10-20 12:23:57 -0500

    raisePower and isEven function

--- Lab6/main.cpp
+++ Lab6/main.cpp
@@ -14,6 +14,9 @@
 using namespace std;
 
 int Summation ( int num1, int num2);
+double raisePower (double n, int p);
+bool isEven ( int a, int b);
+
 
 
 int main() {
@@ -22,10 +25,20 @@ int main() {
 
     cout << "Enter 2 number to find sum:"<< endl;
     cin >> num1 >> num2;
-    //cout << endl;
     cout << "The summation of "<< num1 <<" and "<< num2 << " is " << Summation(num1, num2)<< endl;
     
+    double n;
+    int p=2 ;
+    cout << "Enter a number to find its square:"<< endl;
+    cin >> n;
+    cout <<"The square of "<< n << " is "<< raisePower(n , p)<< endl;
     
+    
+    int a, b;
+    cout << "Enter 2 number to find if they are even:"<< endl;
+    cin >> a >> b;
+    cout << a << " and "<< b << " are even number: "<< isEven(a,b)<< endl;
+
     
     
 
@@ -45,3 +58,24 @@ int Summation ( int num1, int num2)
     int sum;
         return sum = num1 + num2;
     }
+
+
+// RaisePower
+double raisePower( double n, int p)
+{
+    for ( int p =1;p<=1;  p++)
+    {
+        n *=n;
+    }
+    return n;
+}
+
+
+
+//isEven
+bool isEven ( int a, int b)
+{
+    if (a==b)
+        return true;
+    return false;
+}
